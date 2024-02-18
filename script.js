515b9b54d596cbb6e6bf392b5ecaa9a32b819b9c
xdsolraC <xds0lr4c@gmail.com>
2024-02-18 12:42:13 -0400

    JS: add refreshDisplay function

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ let previousValue = "";
 let operator = "";
 let currentValue = "";
 
+function refreshDisplay(val) {
+    const display = document.querySelector(".display");
+    display.textContent = val ? val : currentValue;
+    // limit display to 9 characters
+    if (currentValue.length > 9) {
+        display.textContent = currentValue.slice(0, 9);
+    }
+}
+
+// Default display value
+refreshDisplay("0");
+
 function operate(operator, a, b) {
     switch (operator) {
         case "+":
