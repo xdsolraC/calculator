0fe5aa80b861eba37e39ee0e019ac867c9c6e555
xdsolraC <xds0lr4c@gmail.com>
2024-02-18 12:35:41 -0400

    JS: add operate function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 let previousValue = "";
 let operator = "";
 let currentValue = "";
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
+    }
+}
